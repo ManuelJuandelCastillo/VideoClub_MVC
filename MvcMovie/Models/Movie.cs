35ecca456cd14b5020d06139ca57a03b8de1e076
Manuel del Castillo <zurdodel83@gmail.com>
2019-11-24 03:19:53 -0300

    Agregar archivos de proyecto.

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Titulo")]
+        public String Title { get; set; }
+
+        [Display(Name = "Lanzamiento")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Genero")]
+        public String Genre { get; set; }
+
+        [Display(Name = "Precio")]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Categoria")]
+        public string Rating { get; set; }
+    }
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
